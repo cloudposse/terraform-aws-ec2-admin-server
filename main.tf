2fe685192344b7eb47cd680d9ae557dbc118911a
SweetOps <vladimir.fess@gmail.com>
2017-09-01 18:32:14 +0300

    renamed security group

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "label" {
 }
 
 resource "aws_security_group" "default" {
-  name        = "${module.label.id}"
+  name        = "${module.label.id}-ssh"
   vpc_id      = "${var.vpc_id}"
   description = "Instance default security group (only egress access is allowed)"
 
