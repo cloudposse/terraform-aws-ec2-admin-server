ae7d6815a333a7a2f2b433678df87f84f5785512
SweetOps <vladimir.fess@gmail.com>
2017-09-05 22:11:08 +0300

    updated description for SG

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ module "label" {
 }
 
 resource "aws_security_group" "default" {
-  name        = "${module.label.id}-public_ssh"
+  name        = "${module.label.id}-ssh"
   vpc_id      = "${var.vpc_id}"
-  description = "Instance default security group (only egress access is allowed)"
+  description = "Allowed SSH access from any source"
 
   tags {
     Name      = "${module.label.id}"
