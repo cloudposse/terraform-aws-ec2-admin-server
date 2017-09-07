be6d837ed0707380cded2f418a826e4f34fd85a6
Sergey Vasilyev <s2504s@gmail.com>
2017-09-07 10:24:00 +0300

    Changed the protocol name to tcp

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-    protocol  = "ssh"
+    protocol  = "tcp"
     from_port = 0
     to_port   = 22
 
@@ -73,4 +73,3 @@ module "dns" {
   ttl       = 60
   records   = ["${module.instance.public_hostname}"]
 }
-
